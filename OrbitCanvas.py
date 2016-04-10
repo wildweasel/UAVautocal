aaa369418310cbd3cd906d9738984e096bcd19f8
Matt Kaplan <matt_kaplan77@hotmail.com>
2016-04-10 00:28:39 -0400

    basic flight path

---
+++ OrbitCanvas.py
@@ -0,0 +1,56 @@
+from tkinter import *
+import numpy as np
+import cv2
+from OpenCVCanvas import OpenCVCanvas
+
+class OrbitCanvas(OpenCVCanvas):
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		
+		self.centerX = StringVar()
+		self.centerY = StringVar()
+		self.majorAxis = StringVar()
+		self.minorAxis = StringVar()
+		self.axisYawAngle = StringVar()
+		self.height = StringVar()
+		self.resolution = 10
+		
+	def setResolution(self, resolution):
+		self.resolution = resolution
+		
+	
+	def loadOverhead(self):
+		loaded = self.loadImage()
+		
+		if(loaded):
+			self.rawOverhead = self.img.copy()
+			self.calcFlightPath()
+			
+		return loaded	
+	
+	def calcFlightPath(self):
+		a = float(self.majorAxis.get())
+		b = float(self.minorAxis.get())
+		offsetX = self.rawOverhead.shape[1]/2 + int(self.centerX.get())
+		offsetY = self.rawOverhead.shape[1]/2 - int(self.centerY.get())
+		alpha = float(self.axisYawAngle.get())/180*np.pi
+		self.flightPath = [(int(offsetX+a*np.cos(t)*np.cos(alpha)-b*np.sin(t)*np.sin(alpha)), int(offsetY+a*np.cos(t)*np.sin(alpha)+b*np.sin(t)*np.cos(alpha))) for t in np.linspace(0,2*np.pi,self.resolution)]
+		
+		
+		self.overheadFlightPath = self.rawOverhead.copy()		
+		for pos in self.flightPath:
+			cv2.circle(self.overheadFlightPath, pos, 2, (0, 255, 0), -1)
+		self.publishArray(self.overheadFlightPath)
+		
+	def mapFlightPath(self, currentPosition):
+		
+		currentOverheadPath = self.overheadFlightPath.copy()
+		
+		pos = self.flightPath[currentPosition]
+			
+		cv2.line(currentOverheadPath, (pos[0]-5,pos[1]-5), (pos[0]+5,pos[1]+5), (255,0,0), 3)
+		cv2.line(currentOverheadPath, (pos[0]-5,pos[1]+5), (pos[0]+5,pos[1]-5), (255,0,0), 3)
+		
+		self.publishArray(currentOverheadPath)
+		
--- UAVautocalGUI.py
+++ UAVautocalGUI.py
@@ -6,23 +6,21 @@ import numpy as np
 import sys
 from tkinter import *
 from tkinter import filedialog
-import OpenCVCanvas
+from OpenCVCanvas import OpenCVCanvas
+from OrbitCanvas import OrbitCanvas
 import ButtonState
 import threading
-from Orbit import Orbit
 
 # Set up the orbit step array
 n = 20
-pos = np.linspace(0,2*np.pi,n)
 
 # Default flight parameters
 centerX1 = 0
 centerY1 = 0
-majorAxis1 = 400
-minorAxis1 = 300
+majorAxis1 = 40
+minorAxis1 = 30
 axisYawAngle1 = 0
 height1 = 100
-orbit1 = Orbit(centerX1, centerY1, majorAxis1, minorAxis1, axisYawAngle1, height1)
 
 class UAVautocalGUI(Tk):
 	
@@ -62,70 +60,53 @@ class UAVautocalGUI(Tk):
 		self.delay.set(0)
 		Label(menu1, text = "Speed").pack(side=LEFT)
 		Spinbox(menu1, from_=0, to=1, increment=.1, textvariable=self.delay).pack(side=LEFT)
-				
-		# Orbit 1 major axis length 
-		self.orbit1MajorAxis = StringVar()
-		self.orbit1MajorAxis.set(majorAxis1)
-		Label(menu2, text = "Orbit1 Major Axis Length").pack(side=LEFT)
-		Spinbox(menu2, from_=20, to=500, increment=10, textvariable=self.orbit1MajorAxis, command = lambda: orbit1.setMajorAxis(int(self.orbit1MajorAxis.get()))).pack(side=LEFT)
-
-		# Orbit 1 minor axis length 
-		self.orbit1MinorAxis = StringVar()
-		self.orbit1MinorAxis.set(minorAxis1)
-		Label(menu2, text = "Orbit1 Minor Axis Length").pack(side=LEFT)
-		Spinbox(menu2, from_=20, to=500, increment=10, textvariable=self.orbit1MinorAxis, command = lambda: orbit1.setMajorAxis(int(self.orbit1MinorAxis.get()))).pack(side=LEFT)
-
-		# Orbit 1 center X
-		self.orbit1CenterX = StringVar()
-		self.orbit1CenterX.set(centerX1)
-		Label(menu2, text = "Orbit1 Center X").pack(side=LEFT)
-		Spinbox(menu2, from_=-200, to=200, increment=10, textvariable=self.orbit1CenterX, command = lambda: orbit1.setMajorAxis(int(self.orbit1CenterX.get()))).pack(side=LEFT)
-
-		# Orbit 1 center Y
-		self.orbit1CenterY = StringVar()
-		self.orbit1CenterY.set(centerY1)
-		Label(menu2, text = "Orbit1 Center Y").pack(side=LEFT)
-		Spinbox(menu2, from_=-200, to=200, increment=10, textvariable=self.orbit1CenterY, command = lambda: orbit1.setMajorAxis(int(self.orbit1CenterY.get()))).pack(side=LEFT)
-
-		# Orbit 1 yaw angle
-		self.orbit1AxisYawAngle = StringVar()
-		self.orbit1AxisYawAngle.set(axisYawAngle1)
-		Label(menu2, text = "Orbit1 Yaw Angle").pack(side=LEFT)
-		Spinbox(menu2, from_=0, to=6.28, increment=.1, textvariable=self.orbit1AxisYawAngle, command = lambda: orbit1.setMajorAxis(float(self.orbit1AxisYawAngle.get()))).pack(side=LEFT)
-
-		# Orbit 1 height
-		self.orbit1Height = StringVar()
-		self.orbit1Height.set(height1)
-		Label(menu2, text = "Orbit1 Height").pack(side=LEFT)
-		Spinbox(menu2, from_=40, to=500, increment=20, textvariable=self.orbit1Height, command = lambda: orbit1.setMajorAxis(int(self.orbit1Height.get()))).pack(side=LEFT)
-
-
 		
 		# Display video(s) row
 		videoRow1 = Frame(self)
 		videoRow1.pack()
-		videoRow2 = Frame(self)
-		videoRow2.pack()
-		
+				
 		# Video screens
-		self.videoCanvas1 = OpenCVCanvas.OpenCVCanvas(videoRow1, height=windowHeight, width=windowWidth)
-		self.videoCanvas1.pack(side=LEFT)
+		self.orbitCanvas = OrbitCanvas(videoRow1, height=windowHeight, width=windowWidth)
+		self.orbitCanvas.pack(side=LEFT)
 		
-		self.videoCanvas2 = OpenCVCanvas.OpenCVCanvas(videoRow1, height=windowHeight, width=windowWidth)
+		self.videoCanvas2 = OpenCVCanvas(videoRow1, height=windowHeight, width=windowWidth)
 		self.videoCanvas2.pack(side=LEFT)
 		
-		self.videoCanvas3 = OpenCVCanvas.OpenCVCanvas(videoRow1, height=windowHeight, width=windowWidth)
+		self.videoCanvas3 = OpenCVCanvas(videoRow1, height=windowHeight, width=windowWidth)
 		self.videoCanvas3.pack(side=LEFT)
-		
-		#self.videoCanvas4 = OpenCVCanvas.OpenCVCanvas(videoRow2, height=windowHeight, width=windowWidth)
-		#self.videoCanvas4.pack(side=LEFT)
-		
-		#self.videoCanvas5 = OpenCVCanvas.OpenCVCanvas(videoRow2, height=windowHeight, width=windowWidth)
-		#self.videoCanvas5.pack(side=LEFT)		
-		
-		#self.videoCanvas6 = OpenCVCanvas.OpenCVCanvas(videoRow2, height=windowHeight, width=windowWidth)
-		#self.videoCanvas6.pack(side=LEFT)
-		
+				
+		# Orbit 1 major axis length 
+		self.orbitCanvas.majorAxis.set(majorAxis1)
+		Label(menu2, text = "Orbit1 Major Axis Length").pack(side=LEFT)
+		Spinbox(menu2, from_=20, to=500, increment=10, textvariable=self.orbitCanvas.majorAxis, command=self.orbitCanvas.calcFlightPath).pack(side=LEFT)
+
+		# Orbit 1 minor axis length 
+		self.orbitCanvas.minorAxis.set(minorAxis1)
+		Label(menu2, text = "Orbit1 Minor Axis Length").pack(side=LEFT)
+		Spinbox(menu2, from_=20, to=500, increment=10, textvariable=self.orbitCanvas.minorAxis, command=self.orbitCanvas.calcFlightPath).pack(side=LEFT)
+
+		# Orbit 1 center X
+		self.orbitCanvas.centerX.set(centerX1)
+		Label(menu2, text = "Orbit1 Center X").pack(side=LEFT)
+		Spinbox(menu2, from_=-200, to=200, increment=10, textvariable=self.orbitCanvas.centerX, command=self.orbitCanvas.calcFlightPath).pack(side=LEFT)
+
+		# Orbit 1 center Y
+		self.orbitCanvas.centerY.set(centerY1)
+		Label(menu2, text = "Orbit1 Center Y").pack(side=LEFT)
+		Spinbox(menu2, from_=-200, to=200, increment=10, textvariable=self.orbitCanvas.centerY, command=self.orbitCanvas.calcFlightPath).pack(side=LEFT)
+
+		# Orbit 1 yaw angle
+		self.orbitCanvas.axisYawAngle.set(axisYawAngle1)
+		Label(menu2, text = "Orbit1 Yaw Angle").pack(side=LEFT)
+		Spinbox(menu2, from_=0, to=180, increment=10, textvariable=self.orbitCanvas.axisYawAngle, command=self.orbitCanvas.calcFlightPath).pack(side=LEFT)
+
+		# Orbit 1 height
+		self.orbitCanvas.height.set(height1)
+		Label(menu2, text = "Orbit1 Height").pack(side=LEFT)
+		Spinbox(menu2, from_=40, to=500, increment=20, textvariable=self.orbitCanvas.height, command=self.orbitCanvas.calcFlightPath).pack(side=LEFT)
+
+		self.orbitCanvas.setResolution(n)
+
 		# Where are we in the positional array?
 		self.npos = 0
 		
@@ -138,11 +119,8 @@ class UAVautocalGUI(Tk):
 		if self.buttonState.getState() == ButtonState.ButtonState.State.INIT or \
 		   self.buttonState.getState() == ButtonState.ButtonState.State.LOADED:
 		
-			# Use the deafault tkinter file open dialog
-			self.groundFile = filedialog.askopenfilename()
-		
-			# make sure the user didn't hit 'Cancel'		
-			if self.groundFile:
+			# Get an image
+			if self.orbitCanvas.loadOverhead():
 				self.buttonState.setState(ButtonState.ButtonState.State.LOADED)
 
 		# Action: Pause
@@ -154,6 +132,7 @@ class UAVautocalGUI(Tk):
 			# Reset the images....
 			self.buttonState.setState(ButtonState.ButtonState.State.LOADED)
 			self.npos = 0
+			self.orbitCanvas.calcFlightPath()
 			
 	def runButton(self):
 		
@@ -176,7 +155,7 @@ class UAVautocalGUI(Tk):
 			
 	def flyUAV(self):
 				
-		while self.npos < len(pos):
+		while self.npos < n:
 			
 			# If we're paused, just chill
 			if self.buttonState.getState() == ButtonState.ButtonState.State.PAUSED:
@@ -201,7 +180,8 @@ class UAVautocalGUI(Tk):
 
 	def step(self):
 		
-		print(self.npos)
+		self.orbitCanvas.mapFlightPath(self.npos)
+		
 
 
 
