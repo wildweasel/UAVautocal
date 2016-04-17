fixed camera rotation dynamics

OrbitCamera.py
@@ -21,68 +21,53 @@ class OrbitCamera:
 
 	def moveCamera(self, pos, heading):
 		
-		cameraHeading = heading + self.pan
+		print("heading: ", heading)
 		
-		
-		#groundPlanePos = np.concatenate([pos,[0],[1]])
-		
+		# 3-D World (input overhead image) coords:
+		#	positive x-axis:  East
+		#	positive y-axis:  South
+		#	positive z-axis:  Up		
+		# Camera Translation - where is the UAV?
 		translation = np.array([[1,0,0,-pos[0]],
 								[0,1,0,-pos[1]],
 								[0,0,1,-pos[2]]])
 		
-		
+		# 3-D UAV (camera) coords:  
+		#	positive x-axis:  South
+		#	positive y-axis:  Down
+		#	positive z-axis:  East
 		axesAdj = np.array( [[0, 1, 0],
-							 [0, 0, 1],
+							 [0, 0, -1],
 							 [1, 0, 0]])
-		
-		# Camera Translation - where is the UAV?
-		#translation = np.array([[1,0,pos[0]],
-		#						[0,1,pos[1]],
-		#						[0,0,1]])
-		
 
-								
-		#print(translation)
+		# This camera pans (with heading) first, then tilts, then adjusts up
+		# http://planning.cs.uiuc.edu/node102.html
 		
-		# Camera Principal Axis - pan & tilt the UAV heading
-		
-		# Pan = Yaw; Tilt = Pitch; Up = Roll
-		# R = R_up * R_tilt * R_yaw
-		# R = | (cos a)(cos B)   				      -(sin a)(cos B)  						  sin B 		 |
-		#	  | (sin a)(cos Y)+(cos a)(sin B)(sin Y)   (cos a)(cos Y)-(sin a)(sin B)(sin Y)  -sin Y 		 |
-		#	  | (sin a)(sin Y)-(cos a)(sin B)(cos Y)   (cos a)(sin Y)+(sin a)(sin B)(cos Y)   (cos B)(cos Y) |
-		
-		#a = cameraHeading/180.0*np.pi
-		#B = -self.tilt/180.0*np.pi
-		#Y = self.up/180.0*np.pi
-		
-		# pan is a CW rotation around y-axis, a/k/a negative pitch
-		B = -cameraHeading/180.0*np.pi
-		# tilt is a CW rotation around x-axis, a/k/a negative roll
+		# pan (CW around y-axis) = negative pitch 
+		# incorporates UAV heading (CCW)
+		B = heading - self.pan/180.0*np.pi
+		pitch = np.array([[np.cos(B), 0, np.sin(B)],
+					  [0,1,0],
+					  [-np.sin(B), 0, np.cos(B)]])
+					  
+		# tilt = roll (CCW around x-axis)
 		Y = self.tilt/180.0*np.pi
-		# up is a rotation around z-axis, a/k/a yaw
-		a = self.up/180.0*np.pi
-		
-		#R = np.array([[np.cos(a)*np.cos(B), 							 -np.sin(a)*np.cos(B), 								  np.sin(B)],
-		#			  [np.sin(a)*np.cos(Y)+np.cos(a)*np.sin(B)*np.sin(Y), np.cos(a)*np.cos(Y)+np.sin(a)*np.sin(B)*np.sin(Y), -np.sin(Y)],
-		#			  [np.sin(a)*np.sin(Y)-np.cos(a)*np.sin(B)*np.cos(Y), np.cos(a)*np.sin(Y)+np.sin(a)*np.sin(B)*np.cos(Y),  np.cos(B)*np.cos(Y)]])
-		
-		# tilt only
-		R = np.array([[1,0,0],
+		roll = np.array([[1,0,0],
 					  [0, np.cos(Y), -np.sin(Y)],
 					  [0, np.sin(Y),  np.cos(Y)]])
-		
-		
-		#R = np.array([[1,0,0],
-		#			  [0,0,-1],
-		#			  [0,1,0]])
 					  
-		#print ("R: ", R)
+		# up = yaw (CCW around z-axis)
+		a = self.up/180.0*np.pi
+		yaw = np.array([[np.cos(a), -np.sin(a), 0],
+					  [np.sin(a),  np.cos(a), 0],
+					  [0,0,1]])
+					  
+		R = yaw.dot(roll.dot(pitch))
+
 				
 		extrinsic = R.dot(axesAdj.dot(translation))
 		
 		homography = np.delete(self.intrinsicCameraMatrix.dot(extrinsic), 2, 1)
-		print(pos)
 		
 		#print ("homography", homography)
 		return homography

OrbitCanvas.py
@@ -55,12 +55,14 @@ class OrbitCanvas(OpenCVCanvas):
 		# Flight path equation - parametric equation of elipse
 		#  x = x0 + a * cos t * cos alpha - b * sin t * sin alpha 
 		#  y = y0 + a * cos t * sin alpha + b * sin t * cos alpha 		
-		self.flightPath = [(int(offsetX+a*np.cos(t)*np.cos(alpha)-b*np.sin(t)*np.sin(alpha)), int(offsetY+a*np.cos(t)*np.sin(alpha)+b*np.sin(t)*np.cos(alpha)), height) for t in np.linspace(0,2*np.pi,self.resolution)]
+		self.flightPath = [(int(offsetX+a*np.cos(t)*np.cos(alpha)+b*np.sin(t)*np.sin(alpha)), 
+						   int(offsetY+a*np.cos(t)*np.sin(alpha)-b*np.sin(t)*np.cos(alpha)), height) for t in np.linspace(0,2*np.pi,self.resolution)]
 		
 		# Instantaneous flight headings
 		# dx/dt = - a * sin t * cos alpha - b * cos t * sin alpha 
 		# dy/dt = - a * sin t * sin alpha + b * cos t * cos alpha 
-		self.flightHeadings = [np.arctan2(-a*np.sin(t)*np.cos(alpha)-b*np.cos(t)*np.sin(alpha), -a*np.sin(t)*np.sin(alpha)+b*np.cos(t)*np.cos(alpha)) for t in np.linspace(0,2*np.pi,self.resolution)]
+		self.flightHeadings = [np.arctan2(-a*np.sin(t)*np.sin(alpha)+b*np.cos(t)*np.cos(alpha),
+							   -a*np.sin(t)*np.cos(alpha)-b*np.cos(t)*np.sin(alpha)) for t in np.linspace(0,2*np.pi,self.resolution)]
 		
 		self.orbitCamera.buildCamera(focalLength, (0,0), float(self.cameraPan.get()), float(self.cameraTilt.get()), float(self.cameraUpAngle.get()))
 		
@@ -72,34 +74,38 @@ class OrbitCanvas(OpenCVCanvas):
 		
 	def mapFlightPath(self, currentPosition):
 		
+		# To draw the markers that change as the UAV moves
 		currentOverheadPath = self.overheadFlightPath.copy()
 		
+		# Where are we, and where are we looking?
 		pos = self.flightPath[currentPosition]
 		heading = self.flightHeadings[currentPosition]
 			
+		# Draw a red 'X' at our current position
 		cv2.line(currentOverheadPath, (pos[0]-15,pos[1]-15), (pos[0]+15,pos[1]+15), (255,0,0), 10)
 		cv2.line(currentOverheadPath, (pos[0]-15,pos[1]+15), (pos[0]+15,pos[1]-15), (255,0,0), 10)		
-		
-		
+				
+		# Find the transformation matrix for our current UAV camera view
 		homography = self.orbitCamera.moveCamera(pos, heading)
+				
+		# Corners of the UAV camera view
+		xMax = 400
+		yMax = 300
+		corners = [[0,0,1],[xMax, 0, 1],[xMax, yMax, 1],[0, yMax, 1]]
 		
-		
-		corner = tuple([int(x) for x in inv(homography).dot([400,0,1])[0:2]])
-		corner2 = tuple([int(x) for x in inv(homography).dot([0,0,1])[0:2]])		
-		corner3 = tuple([int(x) for x in inv(homography).dot([0,300,1])[0:2]])
-		corner4 = tuple([int(x) for x in inv(homography).dot([400,300,1])[0:2]])
-		
-		print (corner, corner2, corner3, corner4)
-		
-		cv2.circle(currentOverheadPath, corner, 20, (255,0,255), -1)
-		cv2.circle(currentOverheadPath, corner2, 20, (255,0,255), -1)
-		cv2.circle(currentOverheadPath, corner3, 20, (255,0,255), -1)
-		cv2.circle(currentOverheadPath, corner4, 20, (255,0,255), -1)
+		# Map the camera view corners back to the points on the overhead...
+		invH = inv(homography)		
+		cornersI = [p2eI(invH.dot(x)) for x in corners]
+		# ... so we can show the portion of the overhead we're currently looking at
+		for i in range(len(cornersI)):		
+			cv2.line(currentOverheadPath, cornersI[i], cornersI[i-1], (255,0,255), 3)
 
 		self.publishArray(currentOverheadPath)
 
 		return cv2.warpPerspective(self.rawOverhead, homography, (400,300))
 		
+def p2eI(perspectiveCoord):
+	return tuple([int(x) for x in perspectiveCoord[0:-1]/perspectiveCoord[-1]])
 		
 
 		